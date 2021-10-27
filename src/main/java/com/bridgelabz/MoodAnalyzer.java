28487c851043a89cd30b40961d6f37c7a45bea60
ankushmalode <you@example.com>
2021-10-27 20:01:59 +0530

    able to respond Happy or Sad Mood

---
+++ src/main/java/com/bridgelabz/MoodAnalyzer.java
@@ -0,0 +1,34 @@
+package com.bridgelabz;
+
+public class MoodAnalyzer {
+    private final String message;
+    public MoodAnalyzer(String message){
+        this.message=message;
+    }
+
+    public String analyseMood() throws MoodAnalysisException{
+        try {
+            if (message.equals(""))
+                throw new MoodAnalysisException("Invalid message",MoodAnalysisException.ExceptionType.EXCEPTION_EMPTY);
+            if (message.toLowerCase().contains("sad"))
+                return "SAD";
+            return "HAPPY";
+        } catch (NullPointerException e) {
+            throw new MoodAnalysisException("Invalid message", MoodAnalysisException.ExceptionType.EXCEPTION_NULL);
+        }
+    }
+}
+
+class MoodAnalysisException extends Throwable {
+    public String message;
+    public ExceptionType exceptionType;
+
+    enum ExceptionType{
+        EXCEPTION_NULL, EXCEPTION_EMPTY
+    }
+
+    public MoodAnalysisException(String message, ExceptionType type){
+        this.message=message;
+        this.exceptionType=type;
+    }
+}
